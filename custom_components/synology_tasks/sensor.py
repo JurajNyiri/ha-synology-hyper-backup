c40e2bb2c8f9d1989b8da30d0cc65e90bc8a1112
bckr <bri@bckr.io>
2025-08-30 15:13:33 +0200

    Working DSM API calls to trigger a task

--- custom_components/synology_tasks/sensor.py
+++ custom_components/synology_tasks/sensor.py
@@ -1,5 +1,14 @@
 import requests
 import os
+from typing import TypedDict
+
+
+class SynologyAuthResponse(TypedDict):
+    data: TypedDict(
+        "SynologyAuthData",
+        {"account": str, "device_id": str, "ik_message": str, "is_portal_port": bool, "sid": str, "synotoken": str}
+    )
+    success: bool
 
 def main():
     verify_ssl = os.getenv("VERIFY_SSL", "True") == "True"
@@ -18,6 +27,32 @@ def main():
         "passwd": pw,
     }
 
+    r = s.get(f"{host}/webapi/entry.cgi", params=params, verify=verify_ssl)
+    response: SynologyAuthResponse = r.json()
+    print(r.text)
+    sid = response.get("data").get("sid")
+    print(sid)
+
+    # run the task
+    params = {
+        "api": "SYNO.Entry.Request",
+        "method": "request",
+        "version": "1",
+        "stop_when_error": "false",
+        "mode": "sequential",
+        "compound": "[{\"api\":\"SYNO.Core.EventScheduler\",\"method\":\"run\",\"version\":1,\"task_name\":\"Sync Media\"}]",
+        "SynoToken": response.get("data").get("synotoken")
+    }
+    r = s.get(f"{host}/webapi/entry.cgi", params=params, verify=verify_ssl)
+    print(r.text)
+
+    # logout
+    params = {
+        "api": "SYNO.API.Auth",
+        "version": "6",
+        "method": "logout",
+        "_sid": sid
+    }
     r = s.get(f"{host}/webapi/entry.cgi", params=params, verify=verify_ssl)
     print(r.text)
 
